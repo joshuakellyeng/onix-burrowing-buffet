890b9a8c6385f13822ea20b763a0e4c3f55011b3
Joshua Kelly <joshuakellyeng@gmail.com>
2021-12-19 14:06:39 -0500

    added game assets to import into html5 canvas

---
+++ js/init.js
@@ -0,0 +1,7 @@
+(function () {
+    const overworld = new Overworld({
+        element: document.querySelector('.game-container')
+    })
+    //this is our overworld instance
+    overworld.init();
+})();
