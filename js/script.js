3e2573e06d94a0bde3ef0be2302ebf6a3590ce7e
Joshua Kelly <joshuakellyeng@gmail.com>
2021-12-21 21:06:48 -0500

    establishing randomization for apple object

--- js/script.js
+++ js/script.js
@@ -6,30 +6,13 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 
-class Square {
-	constructor(x, y, w, h) {
-        //set square locations
 
-		this.x = x;
-		this.y = y;
-		this.w = w;
-		this.y = y;
-		this.draw = function () {
-            c.fillStyle = "pink"
-            c.beginPath();
-            c.fillRect()
-		};
-	}
+let pixels = []
+for(let i = 0; i<100 ;i++) {
+    let x = Math.random() * window.innerWidth
+    let y = Math.random() * window.innerHeight
+    c.fillStyle = 'red'
+    pixels.push(c.fillRect(x,y,20,20))
 }
 
-const squareArray = []
-
-for(let i = 0; i < 100; i++) {
-    //set square locations
-    let x = Math.random() * window.innerWidth;
-    let y = Math.random() * window.innerHeight;
-
-    let w = 200;
-    let h = 200;
-    squareArray.push(new Square(x,y,w,h))
-}
+console.log(pixels)
